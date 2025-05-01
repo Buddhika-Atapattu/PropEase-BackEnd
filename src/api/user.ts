0b9fc9e7fa7952cd905e5198882400d014688243
Buddhika-Atapattu <atapattu.buddhika@gmail.com>
2025-05-02 01:57:08 +0530

    feat: :zap: Update user API for verifications

--- src/api/user.ts
+++ src/api/user.ts
@@ -8,6 +8,7 @@ import express, {
 import * as path from "path";
 import * as fs from "fs";
 import { UserModel, IUser } from "../models/user.model";
+import * as Argon2 from "argon2";
 
 export interface Address {
   street: string;
@@ -39,18 +40,18 @@ export interface NewUser extends Document {
   updatedAt: Date;
 }
 
-const dummyUser = new UserModel({
-  firstName: "Buddhika",
-  middleName: "Lahiru",
+const NewAgent = new UserModel({
+  firstName: "Gamin",
+  middleName: "",
   lastName: "Atapattu",
-  username: "admin",
-  email: "alice@example.com",
-  password: "admin1234",
+  username: "agent",
+  email: "agent@example.com",
+  password: "gamini1234",
   age: 28,
   image:
-    "https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=600&auto=format&fit=crop&q=60",
-  phoneNumber: "+94771234567",
-  role: { role: "admin" },
+    "https://images.unsplash.com/photo-1560250097-0b93528c311a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHVzZXJ8ZW58MHx8MHx8fDA%3D",
+  phoneNumber: "+94771234568",
+  role: { role: "agent" },
   address: {
     street: "Maple Street",
     houseNumber: "42B",
@@ -70,31 +71,97 @@ export default class User {
     this.router = Router();
     this.createUser();
     this.getAllUsers();
+    this.getUserData();
     // this.insertDummyUser(); // Insert dummy user immediately when server starts
+  }
+
+  private async hashPassword(password: string): Promise<string> {
+    return await Argon2.hash(password);
+  }
+
+  private async getUserData(): Promise<void> {
+    this.router.post(
+      "/verify-user",
+      async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          console.log(req.body);
+          const user: IUser | null = await UserModel.findOne({
+            username: req.body.username,
+          });
+
+          if (user !== null) {
+            const isPasswordValid = await Argon2.verify(
+              user.password,
+              req.body.password
+            );
+            if (isPasswordValid) {
+              const plainUser = user.toObject ? user.toObject() : user; // fallback if toObject() doesn't exist
+              const { password, ...userWithoutPassword } = plainUser;
+              res.status(200).json(userWithoutPassword);
+            } else {
+              res.status(401).json({ error: "Invalid password" });
+            }
+          } else {
+            res.status(401).json({ error: "Invalid username" });
+          }
+        } catch (error) {
+          res
+            .status(500)
+            .json({ error: "Error getting user data", details: error });
+          next(error);
+        }
+      }
+    );
+    return Promise.resolve();
   }
 
   // private async insertDummyUser() {
   //   try {
-  //     const savedUser = await dummyUser.save();
-  //     console.log("Dummy user inserted:", savedUser);
+  //     // Check if the dummy user already exists in the database
+  //     const existingUser = await UserModel.findOne({ username: "agent" });
+  //     if (existingUser) {
+  //       console.log("Dummy user already exists:", existingUser);
+  //       return; // Exit early if the user already exists
+  //     }
+
+  //     // If the user does not exist, save the dummy user
+  //     const savedUser = await NewAgent.save();
+  //     if (savedUser) {
+  //       console.log("Dummy user inserted:", savedUser);
+  //     } else {
+  //       console.log("Failed to insert dummy user");
+  //     }
   //   } catch (error) {
-  //     console.error("Failed to insert dummy user:", error);
+  //     console.error("Error inserting dummy user:", error);
   //   }
   // }
 
-  private createUser() {
-    this.router.post("/create-user", async (req: Request, res: Response) => {
-      try {
-        const newUser: IUser = new UserModel(req.body);
-        const savedUser = await newUser.save();
-        console.log(req.body);
-        res.status(201).json(savedUser);
-      } catch (error) {
-        res
-          .status(500)
-          .json({ error: "Failed to create user", details: error });
+  private createUser(): Promise<void> {
+    this.router.post(
+      "/create-user",
+      async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          const newUser: IUser = new UserModel(req.body);
+          const existingUser = await UserModel.findOne({
+            username: req.body.username,
+          });
+          if (existingUser) {
+            console.log("User already exists:", existingUser);
+            return; // Exit early if the user already exists
+          } else {
+            const savedUser = await newUser.save();
+            console.log(req.body);
+            res.status(201).json(savedUser);
+          }
+        } catch (error) {
+          res
+            .status(500)
+            .json({ error: "Failed to create user", details: error });
+          next(error);
+        }
       }
-    });
+    );
+    return Promise.resolve();
   }
 
   private getAllUsers() {
